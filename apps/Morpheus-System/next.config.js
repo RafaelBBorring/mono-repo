1ac4d1e54b0477cdf4d8baa1508c0dd66a7d1f89
Rafael <rafael.bborring@gmail.com>
2026-05-06 00:51:27 -0300

    teste att

--- apps/Morpheus-System/next.config.js
+++ apps/Morpheus-System/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/mono-repo/morpheus-system",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
